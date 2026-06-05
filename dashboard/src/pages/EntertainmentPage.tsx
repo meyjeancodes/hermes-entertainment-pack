b812a875bb0e0e0fb4d0787dfbcfda4ad570068a
meyjeancodes <megan14foster@gmail.com>
2026-06-05 13:43:24 -0600

    feat: ch2 → new Twitter video, ch7 → original NousNetwork.mp4

--- dashboard/src/pages/EntertainmentPage.tsx
+++ dashboard/src/pages/EntertainmentPage.tsx
@@ -17,12 +17,12 @@ export interface Channel {
 
 const CHANNELS: Channel[] = [
   { id: "ch1",  name: "Static",      type: "noise",   color: "#111" },
-  { id: "ch2",  name: "Nous Network",type: "video",   src: `${PLUGIN_URL}/media/NousNetwork.mp4`, color: "#0a0a1a" },
+  { id: "ch2",  name: "Nous Network",type: "video",   src: "https://video.twimg.com/amplify_video/2054680803716612096/vid/avc1/1080x1080/FYL3QgQLEZ8ZKJOh.mp4?tag=27", color: "#0a0a1a" },
   { id: "ch3",  name: "Music Scene", type: "iframe",  src: "https://www.youtube.com/embed/NhheiPTdZCw?si=l0t7VslIKlOQ2wTC&controls=1" },
   { id: "ch4",  name: "Weather Retro", type: "iframe", src: `${PLUGIN_URL}/public/weather.html` },
   { id: "ch5",  name: "Nature",      type: "iframe",  src: "https://www.youtube.com/embed/JfKtk3Ch5KA?controls=0&autoplay=1&mute=1", autoplay: true },
   { id: "ch6",  name: "Aethereon",   type: "iframe",  src: "https://www.youtube.com/embed/DdM4_pYLvko?si=Ffw8S3W4U0zEA_Co&controls=0&autoplay=1&mute=1", autoplay: true },
-  { id: "ch7",  name: "Flight Watch",type: "iframe",  src: "https://globe.adsbexchange.com/?lat=39&lon=-30&zoom=3" },
+  { id: "ch7",  name: "Flight Watch",type: "video",   src: `${PLUGIN_URL}/media/NousNetwork.mp4`, color: "#0a0a1a" },
   { id: "ch8",  name: "Local 58",    type: "iframe",  src: "https://www.youtube.com/embed/videoseries?si=ZtbDWE2VlafUuQ0Z&controls=0&list=PLgni59iOLrDCTZB6HV6v349i2e1eyx-0Q&autoplay=1&mute=1", autoplay: true },
   { id: "ch9",  name: "HNN Teletext",type: "canvas",  color: "#000033" },
   { id: "ch10", name: "Vapor FM",    type: "iframe",  src: `${PLUGIN_URL}/public/vapor.html?v=2` },
